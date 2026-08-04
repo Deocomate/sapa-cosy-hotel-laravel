2ee2e6dd460357644d240d5be5953abab15dbefd
minhlong <deocomate@gmail.com>
2026-08-04 17:24:16 +0700

    update

--- app/Http/Controllers/Client/ContactController.php
+++ app/Http/Controllers/Client/ContactController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Client;
 
 use App\Http\Controllers\Controller;
-use Barryvdh\Debugbar\Facades\Debugbar;
 use Illuminate\Http\Request;
 
 class ContactController extends Controller
@@ -11,7 +10,7 @@ class ContactController extends Controller
     public function lien_he()
     {
         $hotelInfo = \DB::table("hotel_info")->first();
-        Debugbar::info($hotelInfo);
         return view("client.modules.lien_he.index");
     }
 }
+
--- app/Http/Controllers/Client/HomeController.php
+++ app/Http/Controllers/Client/HomeController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Client;
 
 use App\Http\Controllers\Controller;
-use Barryvdh\Debugbar\Facades\Debugbar;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -20,7 +19,6 @@ class HomeController extends Controller
     public function bai_viet()
     {
         $blogs = \DB::table("blogs")->get();
-        Debugbar::info($blogs);
         return view("client.modules.bai_viet.index", compact(["blogs"]));
     }
 
@@ -30,14 +28,12 @@ class HomeController extends Controller
         if (!$blog) {
             abort(404, 'Bài viết không tồn tại.');
         }
-        Debugbar::info($blog);
         return view("client.modules.bai_viet.detail", compact(["blog"]));
     }
 
     public function dich_vu()
     {
         $services = \DB::table("services")->get();
-        Debugbar::info($services);
         return view("client.modules.dich_vu.index", compact(["services"]));
     }
 
@@ -47,21 +43,18 @@ class HomeController extends Controller
         if (!$service) {
             abort(404, 'Dịch vụ không tồn tại.');
         }
-        Debugbar::info($service);
         return view("client.modules.dich_vu.detail", compact(["service"]));
     }
 
     public function bai_bao()
     {
         $posts = \DB::table("posts")->get();
-        Debugbar::info($posts);
         return view("client.modules.bai_bao.index", compact(["posts"]));
     }
 
     public function phong_nghi()
     {
         $rooms = \DB::table("rooms")->get();
-        Debugbar::info($rooms);
         return view("client.modules.phong.index", compact(["rooms"]));
     }
 
@@ -74,3 +67,4 @@ class HomeController extends Controller
         return view("client.modules.phong.detail", compact(["room"]));
     }
 }
+
